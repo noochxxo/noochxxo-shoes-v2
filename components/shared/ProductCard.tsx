bdb16a02e987d616708a2355081d592f9e2fd914
noochxxo <18622872+noochxxo@users.noreply.github.com>
2024-04-06 04:59:20 +0000

    The favorites badge is broke

--- components/shared/ProductCard.tsx
+++ components/shared/ProductCard.tsx
@@ -2,8 +2,10 @@
 
 import { useState } from "react";
 import Image from "next/image";
+import { Heart } from 'lucide-react';
 
 import { Card, CardContent } from "../ui/card";
+import { Badge } from "@/components/ui/badge"
 import StarRating from "./StarRating";
 
 const ProductCard = ({ product }: { product: ProductType }) => {
@@ -14,7 +16,17 @@ const ProductCard = ({ product }: { product: ProductType }) => {
     <>
     <Card>
       <CardContent className="justify-center p-0 m-0 ">
-        
+          <Badge variant="secondary" 
+            className="bg-accent hover:bg-accent absolute m-3"
+          >
+            <p
+              className="text-secondary-foreground"
+            >$20 off</p>
+          </Badge>
+
+          <Badge 
+            className="absolute bottom-20 ml-48 bg-card rounded-full size-10 hover:bg-card hover:size-12 "
+          ><Heart className="text-secondary hover:size-12" /></Badge>
           <Image
             src="/images/hero.jpg"
             width={3499}
@@ -22,6 +34,8 @@ const ProductCard = ({ product }: { product: ProductType }) => {
             alt="Shoes"
             className="object-cover rounded-lg"
           />
+
+          
       </CardContent>
     </Card>
     <div
