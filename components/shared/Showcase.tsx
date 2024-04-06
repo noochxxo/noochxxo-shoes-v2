90fddd1208cf12497c90f6126c608ba8df3d4b6d
noochxxo <18622872+noochxxo@users.noreply.github.com>
2024-04-06 22:36:49 +0000

    fixed the width of the entire page overflowing

--- components/shared/Showcase.tsx
+++ components/shared/Showcase.tsx
@@ -8,22 +8,23 @@ import Link from "next/link";
 const Showcase = () => {
   return (
     <section
-      className="mx-8 overflow-hidden max-w-screen grid grid-rows-3 md:grid-cols-6 md:grid-rows-2 gap-4"
+      className="mx-8 overflow-hidden max-w-full grid grid-rows-3 md:grid-cols-6 md:grid-rows-2 gap-4 "
     >
       {products.slice(0, 3).map((product, idx) => {
         return (
           <Link
             key={idx}
             href={`${product.urlBase}${product.id}`}
+            className={`overflow-hidden rounded-lg ${idx === 0 ? 'md:col-span-4 md:row-span-2' : 'md:col-span-2 md:row-span-1'}`}
           >
-            <div  className={`overflow-hidden rounded-lg ${idx === 0 ? 'md:col-span-4 md:row-span-2' : 'md:col-span-2 md:row-span-1'}`}>
+            <div  className={``}>
             <AspectRatio ratio={16 / 9} >
             <Image
               src="/images/hero.jpg"
               width={3499}
               height={5248}
               alt="Shoes"
-              className='object-cover'
+              className='object-cover rounded-lg'
             />
             
           </AspectRatio>
